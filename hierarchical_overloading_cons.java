67b78bd26c44d1144ee41772936a4d8a5016075e
kanjanamalaRajendran <136420306+kanjanamalaRajendran@users.noreply.github.com>
2023-10-20 18:20:53 +0100

    hierarchical_overloading_cons.java

--- hierarchical_overloading_cons.java
+++ hierarchical_overloading_cons.java
@@ -1,3 +1,24 @@
+class College{
+    int a=20;
+    void name(int a,int b){
+    System.out.println(a+b);
+    System.out.println("hi");
+    }
+}
+class  Teacher extends College{
+    void name(int a,int b){
+    System.out.println(a+b);
+    System.out.println("hello");
+}
+}
+class Student extends College{
+    int a=35;
+        void name(int a,int b,int c){
+        System.out.println(a+b);
+        System.out.println("HI hello");
+        }
+}
+
 class Main{
     public static void main(String[] args) {
         Student stu=new Student();
@@ -7,36 +28,4 @@ class Main{
         tea.name(29,28);
         tea.name(24,34);
     }
-}
-class College{
-    int a=20;
-    // College(int a, int b, int c){
-    //     System.out.println(a+b+c);
-    //}
-    void name(int a,int b){
-    System.out.println(a+b);
-    System.out.println("hi");
-    }
-}
-class  Teacher extends College{
-//     Teacher(int a, int b, int c){
-//       System.out.println(a+b+c);
-//   }
-    void name(int a,int b){
-    System.out.println(a+b);
-    System.out.println("hello");
-}
-}
-class Student extends College{
-    int a=35;
-    // Student(int a,int b,int c){
-    //     super(a,b,c);
-    //     System.out.println(a);
-    //     System.out.println(super.a);
-    //     System.out.println(this.a);
-        //}
-        void name(int a,int b,int c){
-        System.out.println(a+b);
-        System.out.println("HI hello");
-        }
 }
