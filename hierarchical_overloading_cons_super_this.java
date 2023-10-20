847e7b07e23acefc2d336607b593ea11e6ae8a2e
kanjanamalaRajendran <136420306+kanjanamalaRajendran@users.noreply.github.com>
2023-10-20 18:12:56 +0100

    hierarchical_overloading_cons_super and this_are using.java

---
+++ hierarchical_overloading_cons_super_this.java
@@ -0,0 +1,42 @@
+
+class Main{
+    public static void main(String[] args) {
+        Student stu=new Student();
+        Teacher tea=new Teacher();
+        stu.name(23,34,34);
+        stu.name(28,34);
+        tea.name(29,28,67);
+        tea.name(24,34);
+    }
+}
+class College{
+    int a=20;
+  
+    void name(int a,int b){
+    System.out.println("hi");
+    System.out.println(a);
+    System.out.println(this.a);
+    System.out.println(a+b);
+    }
+}
+class  Teacher extends College{
+    int a=45;
+    void name(int a,int b,int c){
+    System.out.println("hello");
+    System.out.println(a);
+    System.out.println(this.a);
+    System.out.println(a+b+c);
+    System.out.println(super.a);
+}
+}
+class Student extends College{
+        int a=35;
+    
+        void name(int a,int b,int c){
+        System.out.println("HI hello");
+        System.out.println(a);
+        System.out.println(this.a);
+        System.out.println(a+b+c);
+        System.out.println(super.a);
+        }
+}
