02ac7582c898d1882b03171a6474ccf5d73655b2
kanjanamalaRajendran <136420306+kanjanamalaRajendran@users.noreply.github.com>
2023-10-25 16:35:27 +0100

    Employee.java

--- Employee.java
+++ Employee.java
@@ -1,16 +1,19 @@
 class Employee{
-    int salary ;
-    int age ;
-    String name ;
-    void setdetail(int salary,int age,String name){
+    int salary;
+    int age;
+    String name;
+    void setdetail(int salary,int age,String name)
+    {
         this.salary = salary;
         this.age = age;
         this.name = name;
     }
-    void getdetail(){
+    void getdetail()
+    {
         System.out.println(this.salary+" "+ this.age+" "+  this.name);
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Employee obj = new Employee();
         obj.setdetail(20000,24,"kani");
         obj.getdetail();
